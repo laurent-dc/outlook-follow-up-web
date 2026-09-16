e6cff4d01c77573a8787f8fb126a58f7c73e1f0e
Laurent Decavele <laurentdecavele@MacBook-Air-van-Laurent.local>
2026-09-16 15:02:48 +0200

    Publish resilient Scotty cloud connection

--- auth-complete.js
+++
@@ -1,3 +0,0 @@
-// Clear the token fragment before loading Office.js. Keep it in memory only.
-window.scottyConnection = Object.fromEntries(new URLSearchParams(location.hash.slice(1)));
-history.replaceState(null, '', location.pathname);
